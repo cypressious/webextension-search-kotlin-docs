a5fc8c027d93d50a9d660f4e67a4e8b5cfe217b2
Kirill <kirill.rakhman@gmail.com>
2018-09-30 19:00:12 +0200

    update dependencies and improve code

--- src/main/kotlin/background.kt
+++ src/main/kotlin/background.kt
@@ -1,20 +1,16 @@
 import menus.CreateProperties
 import webextensions.browser
 
+const val URL_SEARCH = "http://kotlinlang.org/?q=[query]&p=0"
+
 fun main(args: Array<String>) {
     browser.menus.create(CreateProperties(
-            id = "search-kotlin",
-            title = "Search in Kotlin Docs",
-            contexts = arrayOf("selection")
+            title = "Search in Kotlin Docs"
     ))
 
-    browser.menus.onClicked.addListener { info, tab ->
-        when (info.menuItemId) {
-            "search-kotlin" -> {
-                browser.tabs.create(tabs.CreateProperties(
-                        url = "http://kotlinlang.org/?q=${info.selectionText}&p=0"
-                ))
-            }
-        }
+    browser.menus.onClicked.addListener { info, _ ->
+        browser.tabs.create(tabs.CreateProperties(
+                url = URL_SEARCH.replace("[query]", info.selectionText ?: return@addListener)
+        ))
     }
 }
